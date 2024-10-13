479ba8e02934c7debc91feccf9d531626dd396aa
gudzsv <gudzsvmail@gmail.com>
2024-10-14 00:35:12 +0300

    modify vite config to compress files in build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import FullReload from 'vite-plugin-full-reload';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    FullReload(['./src/**/*.{js,jsx,tsx,css,json,svg,webp}']),
+    ViteImageOptimizer({
+      exclude: /^sprite.svg$/,
+      png: {
+        quality: 60,
+      },
+      jpeg: {
+        quality: 60,
+      },
+      jpg: {
+        quality: 60,
+      },
+      webp: {
+        quality: 60,
+      },
+    }),
+  ],
+});
