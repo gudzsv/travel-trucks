ea5426e803cb7471304fe7b3bd136596233857ec
gudzsv <gudzsvmail@gmail.com>
2024-10-15 01:28:19 +0300

    added loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,5 +1,16 @@
+import { ColorRing } from 'react-loader-spinner';
 const Loader = () => {
-  return <div className="loader">Loading...</div>;
+  return (
+    <ColorRing
+      visible={true}
+      height="50"
+      width="50"
+      ariaLabel="color-ring-loading"
+      wrapperStyle={{}}
+      wrapperClass="color-ring-wrapper loader"
+      colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+    />
+  );
 };
 
 export default Loader;
